a24eefb68bbaefb09a39d69df3366fd5e892a84c
Azamat <az.berdimuratov@uzinfocom.uz>
2024-07-19 09:17:16 +0500

    first ommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MainLayoutComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {}
